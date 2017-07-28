b468477041a91dbbdfe46cc709d3e350b0f9d09b
whatsupgabe <whatsupgabe@gmail.com>
2017-07-28 14:11:47 -0700

    Add files via upload

--- Pedigree Analysis.cpp
+++ Pedigree Analysis.cpp
@@ -4,34 +4,23 @@
 void ancestors::load_tree(){
 
     ifstream ancestry("Ancestors.txt");
-    int num_ancestor;
-
-    if(!ancestry.is_open()){
-        cout << "error opening file";
-        exit(1);
-    }
-
     vector<int> anAncestor;
+    int num_ancestor;
 
     // read in # of ancestors
     ancestry >> num_ancestor;
-    ancestry.ignore(1000, '\n');
 
-    //cout << "Number of ancestors: " << num_ancestor << endl; // test to make sure # of ancestors read in was correct
-
-    //where I read ancestry from file
+    // where I read ancestry from file
     for (int ii = 0; ii < num_ancestor; ++ii)
     {
         read_ancestry(ancestry, anAncestor);
         family_tree.push_back(anAncestor);
         anAncestor.clear();
-
-        for(int i=0;i<anAncestor.size();++i)
-            cout << anAncestor[i];
-        cout << endl;
     }
 
-    display_tree();
+    vect_to_persons();
+
+    //display_tree();
 }
 
 void ancestors::read_ancestry(ifstream & ancestry, vector<int>  & anAncestor)
@@ -68,8 +57,28 @@ void ancestors::read_ancestry(ifstream & ancestry, vector<int>  & anAncestor)
 }
 
 
+void ancestors::vect_to_persons()
+{
+    person temp_person;
+    for (int ii = 0; ii < family_tree.size(); ++ii)
+    {
+        temp_person.is_carrier = family_tree[ii][0];
+        temp_person.is_parent = family_tree[ii][1];
+        temp_person.has_parent = family_tree[ii][2];
+        temp_person.generation = family_tree[ii][3];
+        temp_person.childNo = family_tree[ii][4];
+        temp_person.parent1No = family_tree[ii][5];
+        temp_person.parent1Gen = family_tree[ii][6];
+        temp_person.parent2No = family_tree[ii][7];
+        temp_person.parent2Gen = family_tree[ii][8];
+        people.push_back(temp_person);
+    }
+}
+
+/*
 void ancestors::display_tree(){
 
+    // displays family_tree vector
     for (int ii = 0; ii < family_tree.size(); ++ii)
     {
         for (int jj = 0; jj < family_tree[ii].size(); ++jj)
@@ -78,7 +87,21 @@ void ancestors::display_tree(){
         }
         cout << endl;
     }
-}
+    // displays people array
+    for (int ii = 0; ii < people.size(); ++ii)
+    {
+        cout << people[ii].is_carrier;
+        cout << people[ii].is_parent;
+        cout << people[ii].has_parent;
+        cout << people[ii].generation;
+        cout << people[ii].childNo;
+        cout << people[ii].parent1No;
+        cout << people[ii].parent1Gen;
+        cout << people[ii].parent2No;
+        cout << people[ii].parent2Gen;
+        cout << endl;
+    }
+}*/
 
 
 int main ()
